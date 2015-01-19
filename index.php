11820fffed1a24920181cbfdc2e50c746ceb4eec
fjonas <731028571@qq.com>
2015-01-19 14:27:32 +0800

    ·
    ·

--- index.php
+++ index.php
@@ -5,11 +5,18 @@ $appid='wx068aede1bc1be5b9';
 $appsecret='f202ac26b747a93bdd0a9fc5275a1a89';
 $wechatObj=new GtWechatApi($appid,$appsecret);
 //$res=$wechatObj->getmenu();
-$data='{"button":[{"name":"个人信息","sub_button":[{"type":"view","name":"注册古太网络","url":"http://www.gt-net.cn/user/register","sub_button":[]},{"type":"view","name":"绑定微信","url":"http://www.gt-net.cn/user/login","sub_button":[]},{"type":"view","name":"服务报价","url":"http://www.gt-net.cn","sub_button":[]}]},{"name":"线上互动","sub_button":[{"type":"view","name":"心愿树","url":"http://www.gt-net.cn","sub_button":[]}]},{"name":"我的应用","sub_button":[{"type":"click","name":"自制电子贺卡","key":"fn_app_card","sub_button":[]}]}]}}{"menu":{"button":[{"name":"个人信息","sub_button":[{"type":"view","name":"注册古太网络","url":"http://www.gt-net.cn/user/register","sub_button":[]},{"type":"view","name":"绑定微信","url":"http://www.gt-net.cn/user/login","sub_button":[]},{"type":"view","name":"服务报价","url":"http://www.gt-net.cn","sub_button":[]}]},{"name":"线上互动","sub_button":[{"type":"view","name":"心愿树","url":"http://www.gt-net.cn","sub_button":[]}]},{"name":"我的应用","sub_button":[{"type":"click","name":"自制电子贺卡","key":"fn_app_card","sub_button":[]}]}]}';
+$data='{
+"button":[
+{"name":"个人信息","sub_button":[
+{"type":"view","name":"注册古太网络","url":"http://www.gt-net.cn/wechat/html/reg.html","sub_button":[]},{"type":"click","name":"绑定微信","key":"fn_band","sub_button":[]},{"type":"view","name":"服务报价","url":"http://www.gt-net.cn","sub_button":[]}]},
+{"name":"线上互动","sub_button":[
+{"type":"view","name":"心愿树","url":"http://www.gt-net.cn","sub_button":[]}]},
+{"name":"我的应用","sub_button":[
+{"type":"click","name":"自制电子贺卡","key":"fn_app_card","sub_button":[]}]}]}}';
 
 $res=$wechatObj->setmenu($data);
 echo '<hr>';
-//var_dump($res);
+var_dump($res);
 echo '<hr>';
 var_dump($wechatObj);
 
